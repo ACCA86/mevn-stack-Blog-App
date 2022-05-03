64f549625baf9331feee679510af0cd5de515d9c
Ibrahim Shaheen <86328711+ACCA86@users.noreply.github.com>
2022-05-03 21:38:48 +0200

    Update mongoDB.js

--- server/mongoDB.js
+++ server/mongoDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 module.exports=()=>{
-    mongoose.connect('mongodb+srv://admin:6861737@cluster0.0ydh0.mongodb.net/test',{
+    mongoose.connect('URL_MongoDB',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>console.log('Connected Database...'))
